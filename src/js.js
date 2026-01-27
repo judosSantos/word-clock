b4c97479a26e081547ed249c2f41e1fc201234f9
judossantos13 <judossantos13@gmail.com>
2026-01-27 11:13:41 +0100

    added HTML, CSS, JS + background

---
+++ src/js.js
@@ -0,0 +1,17 @@
+function pingPongVideo() {
+  const video = document.getElementById("backgroundVideo");
+  let reverse = false;
+
+  video.addEventListener("timeupdate", () => {
+    if (!reverse && video.currentTime >= video.duration - 0.1) {
+      reverse = true;
+    }
+    if (reverse && video.currentTime <= 0.1) {
+      reverse = false;
+    }
+    if (reverse) {
+      video.currentTime -= 0.03; // velocidade de volta
+    }
+  });
+}
+pingPongVideo;
